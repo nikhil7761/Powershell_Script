a3578c0cb9d767f1f766b5ff7c8729ac75d54b39
nikhil.vargude <nikhilvargude@gmail.com>
2024-10-21 13:03:37 +0530

    added poershellscript to transfer data from one bucket to another bucket

---
+++ TransferoneBuckettoanother.ps1
@@ -0,0 +1,15 @@
+﻿# Load the AWS PowerShell module
+Import-Module AWSPowerShell
+
+# Define the source and destination S3 URIs
+$sourceUri = "s3://databasebackuprdsebank/DailyBackupDB/Jathbkp/MONDAYJATH.rar"
+$destinationUri = "s3://databasebackuprdsebank/SixMonthsBackupDb/MONDAYJATH.rar"
+
+# Copy the file from the source to the destination
+Copy-S3Object -SourceBucket "databasebackuprdsebank" `
+               -SourceKey "DailyBackupDB/Jathbkp/MONDAYJATH.rar" `
+               -DestinationBucket "databasebackuprdsebank" `
+               -DestinationKey "SixMonthsBackupDb/MONDAYJATH.rar"
+
+# Output a message indicating completion
+Write-Host "File transferred from $sourceUri to $destinationUri"
